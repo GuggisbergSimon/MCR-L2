5f3d16bc7dccfc4e2c823cdc7ac6266167a61f4a
kumo <patrick.furrer@heig-vd.ch>
2024-04-01 19:09:48 +0200

    Add java doc

--- src/bouncers/ShapeFactory.java
+++ src/bouncers/ShapeFactory.java
@@ -3,8 +3,14 @@ package bouncers;
 
 public interface ShapeFactory {
 
+    /**
+     * Create a square object
+     */
     Square createSquare(int x, int y, int size);
 
+    /**
+     * Create a Circle object
+     */
     Circle createCircle(int x, int y, int speed);
 
 }
